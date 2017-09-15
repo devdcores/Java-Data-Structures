b0f5f2ae2ae8f72575f90e76de80a81df675a568
Devaraj Reddy <devarajreddy4ever@live.com>
2017-09-16 00:10:20 +0530

    Adding stack and linked list implementations.

---
+++ Data Structures in Java/src/Stack/Stack.java
@@ -0,0 +1,55 @@
+package Stack;
+
+public class Stack {
+    private int top;
+    private int size;
+    private String[] stack;
+    public Stack(int size){
+        this.top=-1;
+        this.size=size;
+        stack=new String[this.size];
+    }
+
+    public void push(String element){
+        if(top<size-1) {
+            stack[++top] = element;
+        }
+        else{
+            System.out.println("Stack Overflow!!!!!");
+        }
+    }
+
+    public String pop(){
+        if(top != -1){
+            String topElement = stack[top];
+            stack[top]=null;
+            top--;
+            return topElement;
+        }
+        else
+        {
+            System.out.println("No Element to Return");
+        }
+        return null;
+    }
+    
+    public String peek(){
+        if(top != -1){
+            String topElement = stack[top];
+            return topElement;
+        }
+        else
+        {
+            System.out.println("No Element to Return");
+        }
+        return null;
+    }
+    
+    public boolean isEmpty(){
+        return top == -1;
+    }
+    
+    public String[] allElementsInStack(){
+        return stack;
+    }
+}
---
+++ Data Structures in Java/src/Stack/StackMain.java
@@ -0,0 +1,37 @@
+package Stack;
+
+import java.util.Arrays;
+
+public class StackMain {
+    public static void main(String[] args) {
+        Stack stack =new Stack(5);
+        stack.push("Devaraj");
+        System.out.println("Elements : "+Arrays.asList(stack.allElementsInStack()));
+        
+        stack.push("Reddy");
+        System.out.println("Elements : "+Arrays.asList(stack.allElementsInStack()));
+        System.out.println("Peek Element  : "+stack.peek());
+        
+        stack.pop();
+        System.out.println("Peek Element  : "+stack.peek());
+        
+        stack.pop();
+        System.out.println("Peek Element  : "+stack.peek());
+        
+        stack.pop();
+    
+        stack.push("Devaraj");
+        System.out.println("Elements : "+Arrays.asList(stack.allElementsInStack()));
+        System.out.println("Peek Element  : "+stack.peek());
+    
+        stack.push("rale");
+        System.out.println("Elements : "+Arrays.asList(stack.allElementsInStack()));
+        System.out.println("Peek Element  : "+stack.peek());
+    
+        stack.push("3");
+        stack.push("4");
+        stack.push("5");
+        System.out.println("Elements : "+Arrays.asList(stack.allElementsInStack()));
+        stack.push("6");
+    }
+}
