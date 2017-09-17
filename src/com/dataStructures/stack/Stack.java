5edcd75c4ab27aa05ff44364bc789af327f7f3f9
Devaraj Reddy G <devaraj.reddy@siemens.com>
2017-09-17 13:07:17 +0530

    indent with 2 spaces rather than 2 tabs.

--- src/com/dataStructures/stack/Stack.java
+++ src/com/dataStructures/stack/Stack.java
@@ -7,90 +7,90 @@ import java.util.Arrays;
  * @description : Stack Class
  */
 public class Stack {
-		private int top;
-		private int size;
-		private String[] stack;
-		
-		/**
-		 * @param : size of the stack.
-		 * @description : Initialize the stack object with the maximum size the stack can grow
-		 */ 
-		public Stack(int size) {
-				this.top = -1;
-				this.size = size;
-				stack = new String[this.size];
-		}
-		
-		/**
-		 * @param : element to be added.
-		 * @description : To add/Push an element in to the stack.
-		 * @logic : Add an element in to the stack array after increasing the top. If the top is more or equal to size then print
-		 * stack overflow as it exceeds the size of the stack.
-		 */
-		public void push(String element) {
-				if (top < size - 1) {
-						stack[++top] = element;
-				} else {
-						System.out.println("Stack Overflow!!!!!");
-				}
-		}
-		
-		/**
-		 * @description : To remove/pop an element from the stack.
-		 * @logic : Remove an element and return from the stack, check if array is empty then remove element and decrement the top.
-		 * @return : The top most element.
-		 */
-		public String pop() {
-				if (top != -1) {
-						String topElement = stack[top];
-						stack[top] = null;
-						top--;
-						return topElement;
-				} else {
-						System.out.println("No Element to Return");
-				}
-				return null;
-		}
-		
-		/**
-		 * @description : Return the top most element in the stack.
-		 * @logic : Return the top most element on the stack, if empty stack return print no elements and return null.
-		 * @return : The top most element.
-		 */
-		public String peek() {
-				if (top != -1) {
-						String topElement = stack[top];
-						return topElement;
-				} else {
-						System.out.println("No Element to Return");
-				}
-				return null;
-		}
-		
-		/**
-		 * @description : To check if the stack is empty.
-		 * @logic : If the value of top, if it is -1 then the stack is empty.
-		 * @return : true if the stack is empty else false.
-		 */
-		public boolean isEmpty() {
-				return top == -1;
-		}
-		
-		/**
-		 * @description : Return all the elements in the stack.
-		 * @logic :
-		 * @return all the elements in the stack.
-		 */
-		public String[] getAllElementsInStack() {
-				return stack;
-		}
-		
-		/**
-		 * @description : display the elements in the stack.
-		 */
-		public void display(){
-				System.out.println("************************* Start of Display *******************************");
-				System.out.println("*    Elements in the stack : "+ Arrays.asList(getAllElementsInStack()));
-				System.out.println("************************* End of Display *********************************");
-		}
+  private int top;
+  private int size;
+  private String[] stack;
+  
+  /**
+   * @param : size of the stack.
+   * @description : Initialize the stack object with the maximum size the stack can grow
+   */
+  public Stack(int size) {
+    this.top = -1;
+    this.size = size;
+    stack = new String[this.size];
+  }
+  
+  /**
+   * @param : element to be added.
+   * @description : To add/Push an element in to the stack.
+   * @logic : Add an element in to the stack array after increasing the top. If the top is more or equal to size then print
+   * stack overflow as it exceeds the size of the stack.
+   */
+  public void push(String element) {
+    if (top < size - 1) {
+      stack[++top] = element;
+    } else {
+      System.out.println("Stack Overflow!!!!!");
+    }
+  }
+  
+  /**
+   * @return : The top most element.
+   * @description : To remove/pop an element from the stack.
+   * @logic : Remove an element and return from the stack, check if array is empty then remove element and decrement the top.
+   */
+  public String pop() {
+    if (top != -1) {
+      String topElement = stack[top];
+      stack[top] = null;
+      top--;
+      return topElement;
+    } else {
+      System.out.println("No Element to Return");
+    }
+    return null;
+  }
+  
+  /**
+   * @return : The top most element.
+   * @description : Return the top most element in the stack.
+   * @logic : Return the top most element on the stack, if empty stack return print no elements and return null.
+   */
+  public String peek() {
+    if (top != -1) {
+      String topElement = stack[top];
+      return topElement;
+    } else {
+      System.out.println("No Element to Return");
+    }
+    return null;
+  }
+  
+  /**
+   * @return : true if the stack is empty else false.
+   * @description : To check if the stack is empty.
+   * @logic : If the value of top, if it is -1 then the stack is empty.
+   */
+  public boolean isEmpty() {
+    return top == -1;
+  }
+  
+  /**
+   * @return all the elements in the stack.
+   * @description : Return all the elements in the stack.
+   * @logic :
+   */
+  public String[] getAllElementsInStack() {
+    return stack;
+  }
+  
+  /**
+   * @description : display the elements in the stack.
+   */
+  public void display() {
+    System.out.println("************************* Start of Display *******************************");
+    System.out.println("*    Elements in the stack : " + Arrays.asList(getAllElementsInStack()));
+    System.out.println("************************* End of Display *********************************");
+  }
 }
--- src/com/dataStructures/stack/StackMain.java
+++ src/com/dataStructures/stack/StackMain.java
@@ -5,40 +5,40 @@ package com.dataStructures.stack;
  * @description : StackMain Class
  */
 public class StackMain {
-		
-		public static void main(String[] args) {
-				// Initialize the size of the stack.
-				Stack stack =new Stack(4);
-				
-				System.out.println("check is empty stack : "+stack.isEmpty());
-				
-				System.out.println("$$$$$$$$ pushing some elements $$$$$$$$");
-				//Push some elements into the stack.
-				stack.push("Devaraj");
-				stack.push("Reddy");
-				stack.push("G");
-				
-				//Display the present elements.
-				stack.display();
-				
-				System.out.println("check is empty stack : "+stack.isEmpty());
-				
-				System.out.println("$$$$$$$$ poping some elements $$$$$$$$");
-				
-				//Pop some elements from the stack.
-				stack.pop();
-				stack.pop();
-				
-				//Display the present elements.
-				stack.display();
-				
-				System.out.println("$$$$$$$$ Stack Overflow happens when the limit exceeds. $$$$$$$$");
-				
-				//Push some more elements to show stack overflow.
-				stack.push("bangalore");
-				stack.push("india");
-				stack.push("karnataka");
-				stack.push("kgf");
-				
-		}
+  
+  public static void main(String[] args) {
+    // Initialize the size of the stack.
+    Stack stack = new Stack(4);
+    
+    System.out.println("check is empty stack : " + stack.isEmpty());
+    
+    System.out.println("$$$$$$$$ pushing some elements $$$$$$$$");
+    //Push some elements into the stack.
+    stack.push("Devaraj");
+    stack.push("Reddy");
+    stack.push("G");
+    
+    //Display the present elements.
+    stack.display();
+    
+    System.out.println("check is empty stack : " + stack.isEmpty());
+    
+    System.out.println("$$$$$$$$ poping some elements $$$$$$$$");
+    
+    //Pop some elements from the stack.
+    stack.pop();
+    stack.pop();
+    
+    //Display the present elements.
+    stack.display();
+    
+    System.out.println("$$$$$$$$ Stack Overflow happens when the limit exceeds. $$$$$$$$");
+    
+    //Push some more elements to show stack overflow.
+    stack.push("bangalore");
+    stack.push("india");
+    stack.push("karnataka");
+    stack.push("kgf");
+    
+  }
 }
