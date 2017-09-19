a93004e9db5642dfc172c7ee7c5c7579962d0f39
Devaraj Reddy G <devaraj.reddy@siemens.com>
2017-09-19 23:22:36 +0530

    basic operations on arrays.

--- src/com/dataStructures/arrays/ArrayMain.java
+++ src/com/dataStructures/arrays/ArrayMain.java
@@ -8,5 +8,13 @@ public class ArrayMain {
   
     System.out.println("Array at index 4 : "+customArray.getValueAtIndex(14));
     System.out.println("Does array contain value 14 : "+customArray.doesArrayContainThisValue(14));
+    
+    customArray.deleteIndex(5);
+    customArray.printArray();
+    
+    customArray.insertValue(123);
+    customArray.printArray();
+  
+    System.out.println("Linear Search of 15 : "+customArray.linearSearch(15));
   }
 }
--- src/com/dataStructures/arrays/CustomArray.java
+++ src/com/dataStructures/arrays/CustomArray.java
@@ -11,32 +11,67 @@ public class CustomArray {
   //Insert Some elements in to the array.
   public void generateArray() {
     for (int i = 0; i < size; i++) {
-      array[i]=(int)(Math.random()*10+10);
+      array[i] = (int) (Math.random() * 10 + 10);
     }
   }
   
   //To print the elements in the array.
-  public void printArray(){
+  public void printArray() {
     System.out.println("----------");
     for (int i = 0; i < size; i++) {
-      System.out.println("| "+i+" | " +array[i]+" |");
+      System.out.println("| " + i + " | " + array[i] + " |");
     }
     System.out.println("----------");
   }
   
-  public int getValueAtIndex(int index){
-    if(index < size )
+  public int getValueAtIndex(int index) {
+    if (index < size)
       return array[index];
     return 0;
   }
   
-  public boolean doesArrayContainThisValue(int value){
-    for(int i=0;i<array.length;i++){
-      if(array[i]==value){
+  public boolean doesArrayContainThisValue(int value) {
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == value) {
         return true;
       }
     }
     return false;
   }
+  
+  public void deleteIndex(int index) {
+    if (index < size) {
+      for (int i = index; i < array.length - 1; i++) {
+        array[i] = array[i + 1];
+      }
+      size--;
+    }
+  }
+  
+  public void insertValue(int value) {
+    if (size < 100) {
+      array[size] = value;
+      size++;
+    } else {
+      System.out.println("Array is full !!!!");
+    }
+  }
+  
+  public String linearSearch(int value) {
+    StringBuilder str = new StringBuilder();
+    boolean valueInArray=false;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == value) {
+        str.append(i+" ");
+        valueInArray=true;
+      }
+    }
+    if(!valueInArray){
+      str.append("None");
+    }
+    return str.toString();
+  }
+  
+  
 }
 
